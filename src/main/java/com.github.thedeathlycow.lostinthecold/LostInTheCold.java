9de7b232d08228e001c3887ed1fddff4014812d7
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-03-15 19:27:19 -1000

    Initial commit

---
+++ src/main/java/com.github.thedeathlycow.lostinthecold/LostInTheCold.java
@@ -0,0 +1,15 @@
+package com.github.thedeathlycow.lostinthecold;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LostInTheCold implements DedicatedServerModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
---
+++ src/main/java/com.github.thedeathlycow.lostinthecold/LostInTheColdClient.java
@@ -0,0 +1,10 @@
+package com.github.thedeathlycow.lostinthecold;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LostInTheColdClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
