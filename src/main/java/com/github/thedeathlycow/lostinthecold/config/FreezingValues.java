2ea18877c9af7b7c137010428106578eec592ebe
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-04-05 20:02:27 -1000

    item models and such

--- src/main/java/com/github/thedeathlycow/lostinthecold/config/FreezingValues.java
+++ src/main/java/com/github/thedeathlycow/lostinthecold/config/FreezingValues.java
@@ -2,7 +2,7 @@ package com.github.thedeathlycow.lostinthecold.config;
 
 public class FreezingValues {
     public static final boolean USE_HEART_OVERLAY = false;
-    public static final int SECONDS_PER_FROST_RESIST = 30;
+    public static final int SECONDS_PER_FROST_RESIST = 60;
     public static final int BASE_FROST_RESISTANCE = 4;
     public static final int CHILLY_BIOME_FREEZE_RATE = 1;
     public static final int COLD_BIOME_FREEZE_RATE = 3;
