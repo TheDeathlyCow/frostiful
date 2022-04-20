5b1beaaf718189be241f6ef23d91e873da770f6b
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-04-19 23:32:37 -1000

    fix cold heart overlay not displaying fully when damaged

--- src/main/java/com/github/thedeathlycow/lostinthecold/mixins/client/gui/InGameHudMixin.java
+++ src/main/java/com/github/thedeathlycow/lostinthecold/mixins/client/gui/InGameHudMixin.java
@@ -14,36 +14,57 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(InGameHud.class)
 abstract class InGameHudMixin {
 
     private static final Identifier HEART_OVERLAY_TEXTURE = new Identifier(LostInTheCold.MODID, "textures/gui/cold_heart_overlay.png");
+
+    private final int MAX_COLD_HEARTS = 20;
+    private final int[] heartYPositions = new int[MAX_COLD_HEARTS];
+    private final int[] heartXPositions = new int[MAX_COLD_HEARTS];
 
     @Inject(
             method = "renderHealthBar",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawHeart(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/gui/hud/InGameHud$HeartType;IIIZZ)V",
-                    ordinal = 3,
+                    ordinal = 0,
                     shift = At.Shift.AFTER
             ),
             locals = LocalCapture.CAPTURE_FAILEXCEPTION
     )
-    private void renderFrozenHeart(MatrixStack matrices, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci, InGameHud.HeartType heartType, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, boolean bl3) {
+    private void captureHeartPositions(MatrixStack matrices, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci, InGameHud.HeartType heartType, int i, int j, int k, int l, int m, int n, int o, int p, int q) {
+        if (m < MAX_COLD_HEARTS) {
+            heartYPositions[m] = q;
+            heartXPositions[m] = p;
+        }
+    }
 
+    @Inject(
+            method = "renderHealthBar",
+            at = @At(
+                    value = "TAIL"
+            )
+    )
+    private void drawColdHeartOverlayBar(MatrixStack matrices, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci) {
         double freezingProgress = ((double) player.getFrozenTicks()) / player.getMinFreezeDamageTicks();
 
         freezingProgress = MathHelper.clamp(freezingProgress, 0.0D, 1.0D);
 
         // number of half cold hearts
-        int frozenHealthPoints = (int) (freezingProgress * 20);
+        int frozenHealthPoints = (int) (freezingProgress * MAX_COLD_HEARTS);
 
         // number of whole hearts
         int frozenHealthHearts = MathHelper.ceil((double) frozenHealthPoints / 2.0D);
 
-        if (m < frozenHealthHearts) {
+        for (int m = 0; m < frozenHealthHearts; m++) {
             // is half heart if this is the last heart being rendered and we have an odd
             // number of frozen health points
+            int p = heartXPositions[m];
+            int q = heartYPositions[m];
             boolean isHalfHeart = m + 1 >= frozenHealthHearts && (frozenHealthPoints & 1) == 1;
             this.drawHeartOverLay(matrices, p, q, isHalfHeart);
         }
