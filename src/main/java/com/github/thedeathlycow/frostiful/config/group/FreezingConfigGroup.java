d6383b395cb271331738ad70b3c3012167b99d78
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-17 03:35:31 -1000

    config adjustments

--- src/main/java/com/github/thedeathlycow/frostiful/config/group/FreezingConfigGroup.java
+++ src/main/java/com/github/thedeathlycow/frostiful/config/group/FreezingConfigGroup.java
@@ -9,12 +9,12 @@ public class FreezingConfigGroup extends ConfigItemGroup {
 
     private static final String TRANSLATE_BASE_STRING = "config.frostiful.freezing.";
 
-    public static final DoubleConfigItem BIOME_TEMPERATURE_MULTIPLIER = new DoubleConfigItem("biome_temperature_multiplier", 4.0, TRANSLATE_BASE_STRING + "biome_temperature_multiplier", null, 0.0, Double.POSITIVE_INFINITY);
+    public static final DoubleConfigItem BIOME_TEMPERATURE_MULTIPLIER = new DoubleConfigItem("biome_temperature_multiplier", 2.0, TRANSLATE_BASE_STRING + "biome_temperature_multiplier", null, 0.0, Double.POSITIVE_INFINITY);
     public static final DoubleConfigItem PASSIVE_FREEZING_START_TEMP = new DoubleConfigItem("passive_freezing_start_temp", 0.25, TRANSLATE_BASE_STRING + "passive_freezing_start_temp", null, -2.0, 2.0);
     public static final IntegerConfigItem WET_FREEZE_RATE = new IntegerConfigItem("wet_freeze_rate", 2, TRANSLATE_BASE_STRING + "wet_freeze_rate", null, 0, Integer.MAX_VALUE);
     public static final IntegerConfigItem CANNOT_FREEZE_WARM_RATE = new IntegerConfigItem("cannot_freeze_thaw_rate", 10, TRANSLATE_BASE_STRING + "cannot_freeze_thaw_rate", null, 0, Integer.MAX_VALUE);
     public static final IntegerConfigItem ON_FIRE_THAW_RATE = new IntegerConfigItem("on_fire_thaw_rate", 10, TRANSLATE_BASE_STRING + "on_fire_thaw_rate", null, 0, Integer.MAX_VALUE);
-    public static final IntegerConfigItem WARMTH_PER_LIGHT_LEVEL = new IntegerConfigItem("warmth_per_light_level", 2, TRANSLATE_BASE_STRING + "warmth_per_light_level", null, 0, Integer.MAX_VALUE);
+    public static final IntegerConfigItem WARMTH_PER_LIGHT_LEVEL = new IntegerConfigItem("warmth_per_light_level", 4, TRANSLATE_BASE_STRING + "warmth_per_light_level", null, 0, Integer.MAX_VALUE);
     public static final IntegerConfigItem MIN_WARMTH_LIGHT_LEVEL_DAY = new IntegerConfigItem("min_warmth_light_level_day", 7, TRANSLATE_BASE_STRING + "min_warmth_light_level_day", null, 0, Integer.MAX_VALUE);
     public static final IntegerConfigItem MIN_WARMTH_LIGHT_LEVEL_NIGHT = new IntegerConfigItem("min_warmth_light_level_night", 9, TRANSLATE_BASE_STRING + "min_warmth_light_level_night", null, 0, Integer.MAX_VALUE);
     public static final IntegerConfigItem FREEZE_DAMAGE_AMOUNT = new IntegerConfigItem("freeze_damage_amount", 2, TRANSLATE_BASE_STRING + "freeze_damage_amount", null, 0, Integer.MAX_VALUE);
