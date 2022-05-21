5ef4a61c21a785ce03d5a8a62b7baf2f4e20bdd2
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-20 22:00:01 -1000

    testmod build gradle actually works?

--- src/testmod/java/com/github/thedeathlycow/frostiful/test/icicle/IcicleTests.java
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/icicle/IcicleTests.java
@@ -3,13 +3,13 @@ package com.github.thedeathlycow.frostiful.test.icicle;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
-//import com.github.thedeathlycow.frostiful.block
+import com.github.thedeathlycow.frostiful.block.*;
 
 public class IcicleTests implements FabricGameTest {
 
     @GameTest(structureName = EMPTY_STRUCTURE)
     public void breakIcicle(TestContext context) {
-        //Frost
+        context.setBlockState(0, 0, 0, FrostifulBlocks.ICICLE.getDefaultState());
     }
 
 }
