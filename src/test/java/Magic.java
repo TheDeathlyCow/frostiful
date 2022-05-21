cf4ef20ab5bf95f0df071a287feb7d53a7e706c1
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-20 21:30:11 -1000

    testmod sourceset

---
+++ src/test/java/Magic.java
@@ -0,0 +1,7 @@
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.test.GameTest;
+
+public class Magic implements FabricGameTest {
+
+    Fro
+}
---
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/icicle/IcicleTests.java
@@ -0,0 +1,15 @@
+package com.github.thedeathlycow.frostiful.test.icicle;
+
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+//import com.github.thedeathlycow.frostiful.block
+
+public class IcicleTests implements FabricGameTest {
+
+    @GameTest(structureName = EMPTY_STRUCTURE)
+    public void breakIcicle(TestContext context) {
+        //Frost
+    }
+
+}
