f160004fb09c3056b76e7be218a36e8f4fd7e36e
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-21 00:00:27 -1000

    add frost tipped arrow to dispenser

--- src/main/java/com/github/thedeathlycow/frostiful/block/FrostifulBlocks.java
+++ src/main/java/com/github/thedeathlycow/frostiful/block/FrostifulBlocks.java
@@ -1,11 +1,20 @@
 package com.github.thedeathlycow.frostiful.block;
 
+import com.github.thedeathlycow.frostiful.entity.FrostTippedArrowEntity;
 import com.github.thedeathlycow.frostiful.init.Frostiful;
+import com.github.thedeathlycow.frostiful.item.FrostifulItems;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Position;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class FrostifulBlocks {
 
@@ -15,6 +24,14 @@ public class FrostifulBlocks {
     public static void registerBlocks() {
         register("icicle", ICICLE);
         register("sun_lichen", SUN_LICHEN);
+
+        DispenserBlock.registerBehavior(FrostifulItems.FROST_TIPPED_ARROW, new ProjectileDispenserBehavior() {
+            protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+                FrostTippedArrowEntity arrowEntity = new FrostTippedArrowEntity(world, position.getX(), position.getY(), position.getZ());
+                arrowEntity.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+                return arrowEntity;
+            }
+        });
     }
 
     private static void register(String id, Block block) {
