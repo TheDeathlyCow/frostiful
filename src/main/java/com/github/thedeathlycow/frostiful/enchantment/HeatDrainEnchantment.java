9b02fa9826a96dc7a536607756602deac754ddb8
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-24 02:42:30 -1000

    more base particles for heat drain

--- src/main/java/com/github/thedeathlycow/frostiful/enchantment/HeatDrainEnchantment.java
+++ src/main/java/com/github/thedeathlycow/frostiful/enchantment/HeatDrainEnchantment.java
@@ -62,7 +62,7 @@ public class HeatDrainEnchantment extends Enchantment {
         World world = user.getEntityWorld();
         if (world instanceof ServerWorld serverWorld) {
             Vec3d from = target.getPos();
-            final int numParticles = level * 3;
+            final int numParticles = level * 3 + 5;
 
             double fromX = from.getX();
             double fromY = from.getY();
