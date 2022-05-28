935f80a455fe11e485a93ce73b56e50a128ffed9
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-27 23:55:20 -1000

    polar bear fur tuft

---
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/entity/mob/PolarBearMixin.java
@@ -0,0 +1,8 @@
+package com.github.thedeathlycow.frostiful.mixins.entity.mob;
+
+import net.minecraft.entity.passive.PolarBearEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PolarBearEntity.class)
+public class PolarBearMixin {
+}
