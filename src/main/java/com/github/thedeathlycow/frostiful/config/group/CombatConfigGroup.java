95f59323e8eeb22ee6d6ec59ae68d48b5ecb8051
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-05-29 20:55:05 -1000

    increased heat drain

--- src/main/java/com/github/thedeathlycow/frostiful/config/group/CombatConfigGroup.java
+++ src/main/java/com/github/thedeathlycow/frostiful/config/group/CombatConfigGroup.java
@@ -7,7 +7,7 @@ import com.oroarmor.config.IntegerConfigItem;
 public class CombatConfigGroup extends ConfigItemGroup {
 
     private static final String TRANSLATE_BASE_STRING = "config.frostiful.combat_config.";
-    public static final IntegerConfigItem HEAT_DRAIN_PER_LEVEL = new IntegerConfigItem("heat_drain_per_level", 100, TRANSLATE_BASE_STRING + "heat_drain_per_level");
+    public static final IntegerConfigItem HEAT_DRAIN_PER_LEVEL = new IntegerConfigItem("heat_drain_per_level", 630, TRANSLATE_BASE_STRING + "heat_drain_per_level");
 
     public CombatConfigGroup() {
         super(ImmutableList.of(HEAT_DRAIN_PER_LEVEL), "combat_config");
