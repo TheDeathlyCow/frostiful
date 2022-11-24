18ce0541ded99ad6f4dcaa6148af41bcd3b411b0
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-11-24 00:01:09 -1000

    fix frost data for pathfind test

--- src/testmod/java/com/github/thedeathlycow/frostiful/test/sun_lichen/SunLichenPathfindingTests.java
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/sun_lichen/SunLichenPathfindingTests.java
@@ -1,8 +1,12 @@
 package com.github.thedeathlycow.frostiful.test.sun_lichen;
 
+import com.github.thedeathlycow.frostiful.entity.FreezableEntity;
+import com.github.thedeathlycow.frostiful.util.survival.FrostHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.server.command.TimeCommand;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
@@ -11,16 +15,21 @@ import net.minecraft.util.math.BlockPos;
 public class SunLichenPathfindingTests {
 
     @GameTest(templateName = "frostiful-test:sun_lichen_tests.pathfinding.big")
-    public void villagerDoesNotCollideWithSunLichen(TestContext context) {
+    public void villager_does_not_collide_with_sun_lichen(TestContext context) {
+        context.setTime(1000);
         final BlockPos start = new BlockPos(1, 2, 1);
         final BlockPos end = new BlockPos(10, 2, 7);
         final int freezeAmount = 1000;
 
         final MobEntity entity = context.spawnMob(EntityType.VILLAGER, start);
-        entity.setFrozenTicks(freezeAmount);
-        context.expectEntityWithData(start, EntityType.VILLAGER, Entity::getFrozenTicks, freezeAmount);
+        FrostHelper.addLivingFrost(entity, freezeAmount, false);
+        context.expectEntityWithData(start, EntityType.VILLAGER, (e) -> {
+            return ((FreezableEntity) e).frostiful$getCurrentFrost();
+        }, freezeAmount);
         context.startMovingTowards(entity, end, 0.7f);
-        context.expectEntityWithDataEnd(end, EntityType.VILLAGER, Entity::getFrozenTicks, freezeAmount);
+        context.expectEntityWithDataEnd(end, EntityType.VILLAGER, (e) -> {
+            return ((FreezableEntity) e).frostiful$getCurrentFrost();
+        }, freezeAmount);
     }
 
 }
