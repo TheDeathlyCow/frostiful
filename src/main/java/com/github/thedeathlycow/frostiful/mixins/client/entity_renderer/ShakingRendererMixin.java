b1d86e6b94deb6eb9e7c5845fc47cdd20de00d87
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-11-26 19:52:07 -1000

    shaking config; reduce fur drops from playfight

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/client/entity_renderer/ShakingRendererMixin.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/client/entity_renderer/ShakingRendererMixin.java
@@ -1,11 +1,15 @@
 package com.github.thedeathlycow.frostiful.mixins.client.entity_renderer;
 
+import com.github.thedeathlycow.frostiful.config.FrostifulConfig;
 import com.github.thedeathlycow.frostiful.entity.FreezableEntity;
+import com.github.thedeathlycow.frostiful.init.Frostiful;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +26,8 @@ public class ShakingRendererMixin<T extends LivingEntity, M extends EntityModel<
     )
     private void shakeWhenFreezing(T entity, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof FreezableEntity freezable) {
-            if (freezable.frostiful$isFrozen()) {
+            FrostifulConfig config = Frostiful.getConfig();
+            if (freezable.frostiful$getFrostProgress() >= config.freezingConfig.getShakingStartPercent()) {
                 cir.setReturnValue(true);
             }
         }
