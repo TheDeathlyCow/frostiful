d8285d3c521968fdfb9155bf7ced538dfdd5532f
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-11-27 19:33:47 -1000

    use type check for arrow items

---
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/entity/mob/StrayCarriesFrostArrowMixin.java
@@ -0,0 +1,39 @@
+package com.github.thedeathlycow.frostiful.mixins.entity.mob;
+
+import com.github.thedeathlycow.frostiful.item.FItems;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
+
+@Mixin(HostileEntity.class)
+public abstract class StrayCarriesFrostArrowMixin extends PathAwareEntity {
+
+    protected StrayCarriesFrostArrowMixin(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(
+            method = "getArrowType",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void straysHaveFrostArrows(ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+        if (this.getType() == EntityType.STRAY && stack.getItem() instanceof RangedWeaponItem rangedWeaponItem) {
+            Predicate<ItemStack> isItemAmmoTest = rangedWeaponItem.getHeldProjectiles();
+            ItemStack heldStack = RangedWeaponItem.getHeldProjectile(this, isItemAmmoTest);
+            if (heldStack.isEmpty()) {
+                cir.setReturnValue(new ItemStack(FItems.FROST_TIPPED_ARROW));
+            }
+        }
+    }
+
+}
