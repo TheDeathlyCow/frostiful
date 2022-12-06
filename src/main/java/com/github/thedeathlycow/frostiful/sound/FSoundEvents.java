a1c414bcc07e1d89641188881f6b550221b2dbac
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-12-05 23:23:24 -1000

    fix kick bug with frost wand

--- src/main/java/com/github/thedeathlycow/frostiful/sound/FSoundEvents.java
+++ src/main/java/com/github/thedeathlycow/frostiful/sound/FSoundEvents.java
@@ -10,9 +10,7 @@ public class FSoundEvents {
     public static final SoundEvent FIRE_LICHEN_DISCHARGE = FSoundEvents.of("block.frostiful.sun_lichen.discharge");
     public static final SoundEvent CAMPFIRE_HISS = FSoundEvents.of("block.frostiful.campfire.hiss");
     public static final SoundEvent ITEM_FROST_WAND_CAST_SPELL = FSoundEvents.of("item.frostiful.frost_wand.cast_spell");
-
     public static final SoundEvent ITEM_FROST_WAND_PREPARE_CAST = FSoundEvents.of("item.frostiful.frost_wand.prepare_cast");
-
     public static final SoundEvent ENTITY_FROST_SPELL_FREEZE = FSoundEvents.of("entity.frostiful.frost_spell.freeze");
 
 
@@ -20,6 +18,7 @@ public class FSoundEvents {
         register(FIRE_LICHEN_DISCHARGE);
         register(CAMPFIRE_HISS);
         register(ITEM_FROST_WAND_CAST_SPELL);
+        register(ITEM_FROST_WAND_PREPARE_CAST);
         register(ENTITY_FROST_SPELL_FREEZE);
     }
 
