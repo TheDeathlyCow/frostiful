996e1434859faf2c918a99bc2833cac6a79d3b53
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-12-22 02:25:16 -1000

    powder snow walking tests

---
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/powder_snow_walkable/EntityPowderSnowTests.java
@@ -0,0 +1,97 @@
+package com.github.thedeathlycow.frostiful.test.powder_snow_walkable;
+
+import com.github.thedeathlycow.frostiful.item.FItems;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.command.ItemCommand;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+import net.minecraft.util.math.BlockPos;
+
+@SuppressWarnings("unused")
+public class EntityPowderSnowTests {
+
+    @GameTest(templateName = "frostiful-test:powder_snow_walkable_test")
+    public void zombie_wearing_fur_boots_does_not_fall(TestContext context) {
+        context.setTime(18000);
+        BlockPos spawnPos = new BlockPos(1, 3, 1);
+
+        ZombieEntity zombie = context.spawnEntity(EntityType.ZOMBIE, spawnPos);
+        StackReference stackReference = zombie.getStackReference(EquipmentSlot.FEET.getOffsetEntitySlotId(100));
+        stackReference.set(new ItemStack(FItems.FUR_BOOTS));
+
+        context.waitAndRun(
+                20,
+                () -> {
+                    context.dontExpectEntityAtEnd(EntityType.ZOMBIE, new BlockPos(1, 2, 1));
+                }
+        );
+    }
+
+    @GameTest(templateName = "frostiful-test:powder_snow_walkable_test")
+    public void zombie_wearing_chainmail_fur_boots_does_not_fall(TestContext context) {
+        context.setTime(18000);
+        BlockPos spawnPos = new BlockPos(1, 3, 1);
+
+        ZombieEntity zombie = context.spawnEntity(EntityType.ZOMBIE, spawnPos);
+        StackReference stackReference = zombie.getStackReference(EquipmentSlot.FEET.getOffsetEntitySlotId(100));
+        stackReference.set(new ItemStack(FItems.FUR_PADDED_CHAINMAIL_BOOTS));
+        context.waitAndRun(
+                20,
+                () -> {
+                    context.dontExpectEntityAtEnd(EntityType.ZOMBIE, new BlockPos(1, 2, 1));
+                }
+        );
+    }
+
+    @GameTest(templateName = "frostiful-test:powder_snow_walkable_test")
+    public void zombie_wearing_leather_boots_does_not_fall(TestContext context) {
+        context.setTime(18000);
+        BlockPos spawnPos = new BlockPos(1, 3, 1);
+
+        ZombieEntity zombie = context.spawnEntity(EntityType.ZOMBIE, spawnPos);
+        StackReference stackReference = zombie.getStackReference(EquipmentSlot.FEET.getOffsetEntitySlotId(100));
+        stackReference.set(new ItemStack(Items.LEATHER_BOOTS));
+
+        context.waitAndRun(
+                20,
+                () -> {
+                    context.dontExpectEntityAtEnd(EntityType.ZOMBIE, new BlockPos(1, 2, 1));
+                }
+        );
+    }
+
+    @GameTest(templateName = "frostiful-test:powder_snow_walkable_test")
+    public void zombie_wearing_no_boots_falls(TestContext context) {
+        context.setTime(18000);
+        BlockPos spawnPos = new BlockPos(1, 3, 1);
+
+        ZombieEntity zombie = context.spawnEntity(EntityType.ZOMBIE, spawnPos);
+
+        context.waitAndRun(
+                20,
+                () -> {
+                    context.expectEntityAtEnd(EntityType.ZOMBIE, new BlockPos(1, 2, 1));
+                }
+        );
+    }
+
+    @GameTest(templateName = "frostiful-test:powder_snow_walkable_test")
+    public void rabbit_does_not_fall(TestContext context) {
+        BlockPos spawnPos = new BlockPos(1, 3, 1);
+
+        RabbitEntity rabbit = context.spawnEntity(EntityType.RABBIT, spawnPos);
+
+        context.waitAndRun(
+                20,
+                () -> {
+                    context.dontExpectEntityAtEnd(EntityType.RABBIT, new BlockPos(1, 2, 1));
+                }
+        );
+    }
+}
