2c8213fac6fdd9b06cd663bc205dad943a7f498a
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-01-01 03:09:43 -1000

    reorder creative tab a bit

--- src/main/java/com/github/thedeathlycow/frostiful/item/FItemGroups.java
+++ src/main/java/com/github/thedeathlycow/frostiful/item/FItemGroups.java
@@ -10,6 +10,7 @@ public class FItemGroups {
     public static final ItemGroup FROSTIFUL = FabricItemGroupBuilder.create(Frostiful.id("frostiful"))
             .icon(() -> new ItemStack(FItems.FROST_WAND))
             .appendItems(stacks -> {
+                stacks.add(new ItemStack(FItems.FROST_WAND));
                 stacks.add(new ItemStack(FItems.FUR_HELMET));
                 stacks.add(new ItemStack(FItems.FUR_CHESTPLATE));
                 stacks.add(new ItemStack(FItems.FUR_LEGGINGS));
@@ -25,14 +26,6 @@ public class FItemGroups {
                 stacks.add(new ItemStack(FItems.COOL_SUN_LICHEN));
                 stacks.add(new ItemStack(FItems.WARM_SUN_LICHEN));
                 stacks.add(new ItemStack(FItems.HOT_SUN_LICHEN));
-
-                stacks.add(new ItemStack(FItems.FROST_WAND));
-                stacks.add(new ItemStack(FItems.FROSTOLOGER_SPAWN_EGG));
-                stacks.add(new ItemStack(FItems.CHILLAGER_SPAWN_EGG));
-                stacks.add(new ItemStack(FItems.ICICLE));
-                stacks.add(new ItemStack(FItems.FROST_TIPPED_ARROW));
-
-                stacks.add(new ItemStack(FItems.FROZEN_TORCH));
 
                 stacks.add(new ItemStack(FItems.PACKED_SNOW));
                 stacks.add(new ItemStack(FItems.PACKED_SNOW_BLOCK));
@@ -51,6 +44,12 @@ public class FItemGroups {
                 stacks.add(new ItemStack(FItems.CUT_BLUE_ICE_STAIRS));
                 stacks.add(new ItemStack(FItems.CUT_BLUE_ICE_SLAB));
                 stacks.add(new ItemStack(FItems.CUT_BLUE_ICE_WALL));
+
+                stacks.add(new ItemStack(FItems.FROSTOLOGER_SPAWN_EGG));
+                stacks.add(new ItemStack(FItems.CHILLAGER_SPAWN_EGG));
+                stacks.add(new ItemStack(FItems.ICICLE));
+                stacks.add(new ItemStack(FItems.FROST_TIPPED_ARROW));
+                stacks.add(new ItemStack(FItems.FROZEN_TORCH));
             }).build();
 
 }
