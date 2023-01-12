9794d627a62010c768b213bf99fe50e90546ff36
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-01-11 23:46:25 -1000

    melt damage is not fire

--- src/main/java/com/github/thedeathlycow/frostiful/entity/damage/FDamageSource.java
+++ src/main/java/com/github/thedeathlycow/frostiful/entity/damage/FDamageSource.java
@@ -1,14 +1,11 @@
 package com.github.thedeathlycow.frostiful.entity.damage;
 
 import com.github.thedeathlycow.frostiful.init.Frostiful;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.EntityDamageSource;
-import org.jetbrains.annotations.Nullable;
 
 public class FDamageSource extends DamageSource { // extend DamageSource to access its constructor
 
-    public static final DamageSource MELT = new FDamageSource(Frostiful.MODID + ".melt").setFire();
+    public static final DamageSource MELT = new FDamageSource(Frostiful.MODID + ".melt");
     public static final DamageSource FALLING_ICICLE = new FDamageSource(Frostiful.MODID + ".fallingIcicle").setFallingBlock();
     public static final DamageSource ICICLE = new FDamageSource(Frostiful.MODID + ".icicle").setFromFalling();
 
