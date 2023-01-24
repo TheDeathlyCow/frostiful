1f62dd94ef9e4f0e1b39ced405a77df212f566bb
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-01-23 20:59:05 -1000

    fix sounds; make rooted entities fully immobile

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/entity/RootedEntityImplMixin.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/entity/RootedEntityImplMixin.java
@@ -20,6 +20,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(LivingEntity.class)
@@ -68,6 +69,17 @@ public abstract class RootedEntityImplMixin extends Entity implements RootedEnti
     }
 
     @Inject(
+            method = "isImmobile",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void rootedEntitiesAreImmobile(CallbackInfoReturnable<Boolean> cir) {
+        if (this.frostiful$isRooted()) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(
             method = "tickMovement",
             at = @At(
                     value = "INVOKE",
@@ -90,11 +102,6 @@ public abstract class RootedEntityImplMixin extends Entity implements RootedEnti
         }
 
         if (this.frostiful$isRooted()) {
-            final LivingEntity instance = (LivingEntity) (Object) this;
-            instance.setJumping(false);
-            instance.sidewaysSpeed = 0.0F;
-            instance.forwardSpeed = 0.0F;
-
             // remove tick
             int ticksLeft = this.frostiful$getRootedTicks();
             this.frostiful$setRootedTicks(--ticksLeft);
