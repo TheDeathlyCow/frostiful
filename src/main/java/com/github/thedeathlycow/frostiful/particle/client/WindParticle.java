7779bf5bc00a942c9a58794e81ba7aa9797cc818
Michael Alpenfels <mwa172@uclive.ac.nz>
2023-01-28 03:04:56 -1000

    wind particles dont always face camera

--- src/main/java/com/github/thedeathlycow/frostiful/particle/client/WindParticle.java
+++ src/main/java/com/github/thedeathlycow/frostiful/particle/client/WindParticle.java
@@ -4,11 +4,23 @@ import com.github.thedeathlycow.frostiful.particle.WindParticleEffect;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.*;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 @Environment(EnvType.CLIENT)
 public class WindParticle extends SpriteBillboardParticle {
+
+    private static final Vec3f FROM = Util.make(new Vec3f(0.5F, 0.5F, 0.5F), Vec3f::normalize);
+    private static final Vec3f TO = new Vec3f(-1.0F, -1.0F, 0.0F);
 
     private final SpriteProvider spriteProvider;
 
@@ -16,12 +28,68 @@ public class WindParticle extends SpriteBillboardParticle {
         super(clientWorld, x, y, z);
         this.spriteProvider = spriteProvider;
         this.velocityX *= 2;
+        this.scale *= 2;
         this.setSpriteForAge(spriteProvider);
     }
 
+    @Override
     public void tick() {
         super.tick();
         this.setSpriteForAge(this.spriteProvider);
+    }
+
+    @Override
+    public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
+        this.buildGeometry(vertexConsumer, camera, tickDelta, (quaternion) -> {
+            quaternion.hamiltonProduct(Vec3f.POSITIVE_Y.getRadialQuaternion(0));
+        });
+        this.buildGeometry(vertexConsumer, camera, tickDelta, (quaternion) -> {
+            quaternion.hamiltonProduct(Vec3f.POSITIVE_Y.getRadialQuaternion(-MathHelper.PI));
+        });
+    }
+
+    private void buildGeometry(
+            VertexConsumer vertexConsumer,
+            Camera camera,
+            float tickDelta,
+            Consumer<Quaternion> rotator
+    ) {
+        Vec3d cameraPos = camera.getPos();
+        float dx = (float) (MathHelper.lerp(tickDelta, this.prevPosX, this.x) - cameraPos.getX());
+        float dy = (float) (MathHelper.lerp(tickDelta, this.prevPosY, this.y) - cameraPos.getY());
+        float dz = (float) (MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - cameraPos.getZ());
+        Quaternion quaternion = new Quaternion(FROM, 0.0F, true);
+        rotator.accept(quaternion);
+        TO.rotate(quaternion);
+        Vec3f[] points = new Vec3f[]{
+                new Vec3f(-1.0F, -1.0F, 0.0F),
+                new Vec3f(-1.0F, 1.0F, 0.0F),
+                new Vec3f(1.0F, 1.0F, 0.0F),
+                new Vec3f(1.0F, -1.0F, 0.0F)
+        };
+
+        float size = this.getSize(tickDelta);
+
+        for (int i = 0; i < 4; ++i) {
+            Vec3f point = points[i];
+            point.rotate(quaternion);
+            point.scale(size);
+            point.add(dx, dy, dz);
+        }
+
+        int brightness = this.getBrightness(tickDelta);
+        this.vertex(vertexConsumer, points[0], this.getMaxU(), this.getMaxV(), brightness);
+        this.vertex(vertexConsumer, points[1], this.getMaxU(), this.getMinV(), brightness);
+        this.vertex(vertexConsumer, points[2], this.getMinU(), this.getMinV(), brightness);
+        this.vertex(vertexConsumer, points[3], this.getMinU(), this.getMaxV(), brightness);
+    }
+
+    private void vertex(VertexConsumer vertexConsumer, Vec3f pos, float u, float v, int light) {
+        vertexConsumer.vertex(pos.getX(), pos.getY(), pos.getZ())
+                .texture(u, v)
+                .color(this.red, this.green, this.blue, this.alpha)
+                .light(light)
+                .next();
     }
 
     @Override
