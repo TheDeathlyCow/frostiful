f6c0a08d7fa1b032ac6c909b285bb56867b7c0e5
Michael <mwa172@uclive.ac.nz>
2023-02-26 12:30:34 +1300

    environment annotation for frost overlay renderer

--- src/main/java/com/github/thedeathlycow/frostiful/client/gui/FrostOverlayRenderer.java
+++ src/main/java/com/github/thedeathlycow/frostiful/client/gui/FrostOverlayRenderer.java
@@ -3,11 +3,14 @@ package com.github.thedeathlycow.frostiful.client.gui;
 import com.github.thedeathlycow.frostiful.config.FrostifulConfig;
 import com.github.thedeathlycow.frostiful.init.Frostiful;
 import com.github.thedeathlycow.frostiful.item.FItems;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
 
 import java.util.function.Consumer;
 
+@Environment(EnvType.CLIENT)
 public final class FrostOverlayRenderer {
 
     private FrostOverlayRenderer() {
@@ -44,7 +47,7 @@ public final class FrostOverlayRenderer {
 
         if (freezeScale >= renderThreshold) {
             // scale opacity to temp scale
-            float opacity = renderThreshold == 1.0f
+            float opacity = renderThreshold >= 1.0f
                     ? 0.0f
                     : (freezeScale - renderThreshold) / (1.0f - renderThreshold);
             renderCallback.accept(opacity);
