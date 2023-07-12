5a5472959066c7692812be7154952b113d074ae3
TheDeathlyCow <thedeathlycow@gmail.com>
2023-07-12 02:51:36 -1000

    trying to make a compatible trim pattern

---
+++ src/main/java/com/github/thedeathlycow/frostiful/client/FTexturedRenderLayers.java
@@ -0,0 +1,25 @@
+package com.github.thedeathlycow.frostiful.client;
+
+import com.github.thedeathlycow.frostiful.Frostiful;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.TexturedRenderLayers;
+import net.minecraft.util.Identifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FTexturedRenderLayers {
+
+    public static final Identifier ARMOR_TRIMS_ATLAS_TEXTURE = Frostiful.id("textures/atlas/armor_trims.png");
+    public static final RenderLayer ARMOR_TRIMS_RENDER_LAYER = RenderLayer.getArmorCutoutNoCull(ARMOR_TRIMS_ATLAS_TEXTURE);
+    public static final Map<Identifier, Identifier> LAYERS_TO_LOADERS = new HashMap<>();
+
+    private FTexturedRenderLayers() {
+
+    }
+
+    static {
+        LAYERS_TO_LOADERS.put(ARMOR_TRIMS_ATLAS_TEXTURE, new Identifier("armor_trims"));
+    }
+
+}
---
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/client/SpriteAtlasManagerMixin.java
@@ -0,0 +1,28 @@
+package com.github.thedeathlycow.frostiful.mixins.client;
+
+import com.github.thedeathlycow.frostiful.client.FTexturedRenderLayers;
+import net.minecraft.client.render.model.SpriteAtlasManager;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(SpriteAtlasManager.class)
+public class SpriteAtlasManagerMixin {
+
+    @ModifyVariable(
+            method = "<init>",
+            argsOnly = true,
+            at = @At("HEAD")
+    )
+    private static Map<Identifier, Identifier> setAtlases(Map<Identifier, Identifier> loaders) {
+        Map<Identifier, Identifier> mutableLoaders = new HashMap<>();
+        mutableLoaders.putAll(FTexturedRenderLayers.LAYERS_TO_LOADERS);
+        mutableLoaders.putAll(loaders);
+        return loaders;
+    }
+
+}
