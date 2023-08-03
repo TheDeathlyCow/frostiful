37bd0a2d06b7bf1aaedaf228c152c766bcf6deea
TheDeathlyCow <thedeathlycow@gmail.com>
2023-08-04 01:51:41 +1200

    the most disgusting reflection hack ever
    this is gross omg
    
    Co-Authored-By: Nerjal Nosk <nosk.nerjal@gmx.com>

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/compat/overflowingbars/present/FreezingHearts.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/compat/overflowingbars/present/FreezingHearts.java
@@ -1,23 +1,69 @@
 package com.github.thedeathlycow.frostiful.mixins.compat.overflowingbars.present;
 
-import net.minecraft.client.gui.hud.InGameHud;
+import com.github.thedeathlycow.frostiful.Frostiful;
+import fuzs.overflowingbars.client.handler.HealthBarRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(InGameHud.HeartType.class)
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+@Mixin(targets = "fuzs.overflowingbars.client.handler.HealthBarRenderer$HeartType")
 public abstract class FreezingHearts {
 
+    private static Object frostiful$FrozenHeartType;
+
+    /**
+     * Lazily instantiate {@link #frostiful$FrozenHeartType}. The return value should be a reference to
+     * {@link fuzs.overflowingbars.client.handler.HealthBarRenderer.HeartType#FROZEN}, but because the class is package
+     * private and a reference cannot be returned directly
+     *
+     * @return Returns the enum field {@link fuzs.overflowingbars.client.handler.HealthBarRenderer.HeartType#FROZEN}, but
+     * as an object reference
+     */
+    private static Object getFrozenHeartField() {
+        try {
+            // yes, this is disgusting, but the class is package private so here we are
+            // :)
+            Field field = Arrays.stream(HealthBarRenderer.class.getDeclaredClasses())
+                    .filter(c -> {
+                        return c.getSimpleName().equals("HeartType");
+                    })
+                    .findFirst()
+                    .orElseThrow()
+                    .getField("FROZEN");
+
+            if (field.trySetAccessible()) {
+                return field.get(null);
+            } else {
+                return null;
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @Inject(
-            method = "fromPlayerState",
+            method = "forPlayer",
             at = @At("TAIL"),
             cancellable = true
     )
-    private static void setFreezingHeartsWhenFrozen(PlayerEntity player, CallbackInfoReturnable<InGameHud.HeartType> cir) {
+    private static void setFreezingHeartsWhenFrozen(
+            PlayerEntity player,
+            boolean absorbing,
+            boolean orange,
+            CallbackInfoReturnable<Object> cir
+    ) {
         if (player.thermoo$getTemperatureScale() <= -0.99) {
-            cir.setReturnValue(InGameHud.HeartType.FROZEN);
+            if (frostiful$FrozenHeartType == null) {
+                frostiful$FrozenHeartType = getFrozenHeartField();
+            }
+
+            cir.setReturnValue(frostiful$FrozenHeartType);
         }
     }
 
