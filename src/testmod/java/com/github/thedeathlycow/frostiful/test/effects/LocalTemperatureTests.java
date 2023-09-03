f26d4a64665fb79f04f59bfe10e179dc1251c1a6
TheDeathlyCow <thedeathlycow@gmail.com>
2023-09-03 01:20:46 -1000

    add test to verify villager is not warmed without a torch

--- src/testmod/java/com/github/thedeathlycow/frostiful/test/effects/LocalTemperatureTests.java
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/effects/LocalTemperatureTests.java
@@ -2,6 +2,7 @@ package com.github.thedeathlycow.frostiful.test.effects;
 
 import com.github.thedeathlycow.thermoo.api.temperature.EnvironmentController;
 import com.github.thedeathlycow.thermoo.api.temperature.EnvironmentManager;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.VillagerEntity;
@@ -28,6 +29,8 @@ public class LocalTemperatureTests {
                 VillagerEntity::thermoo$getTemperature,
                 temperature
         );
+
+        context.expectBlock(Blocks.TORCH, pos);
 
         context.waitAndRun(
                 5L,
@@ -71,11 +74,45 @@ public class LocalTemperatureTests {
                 boat.getId()
         );
 
+        context.expectBlock(Blocks.TORCH, pos);
+
         context.waitAndRun(
                 5L,
                 () -> {
                     context.addInstantFinalTask(() -> context.assertTrue(
                             villager.thermoo$getTemperature() > temperature,
+                            "Villager must be warmed"
+                    ));
+                }
+        );
+    }
+
+
+    @GameTest(templateName = "frostiful-test:effects.local_temperature")
+    public void villager_is_not_warmed(TestContext context) {
+        BlockPos pos = new BlockPos(1, 2, 1);
+        int temperature = -5300;
+
+        EnvironmentController controller = EnvironmentManager.INSTANCE.getController();
+
+        VillagerEntity villager = context.spawnMob(EntityType.VILLAGER, pos);
+        villager.thermoo$setTemperature(temperature);
+
+        context.expectEntityWithData(
+                pos,
+                EntityType.VILLAGER,
+                VillagerEntity::thermoo$getTemperature,
+                temperature
+        );
+
+        context.setBlockState(pos, Blocks.AIR.getDefaultState());
+        context.dontExpectBlock(Blocks.TORCH, pos);
+
+        context.waitAndRun(
+                5L,
+                () -> {
+                    context.addInstantFinalTask(() -> context.assertTrue(
+                            villager.thermoo$getTemperature() == temperature,
                             "Villager must be warmed"
                     ));
                 }
