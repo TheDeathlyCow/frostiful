a689c8f87a28c0f67efde7981ce7f0bb3151f369
TheDeathlyCow <thedeathlycow@gmail.com>
2023-09-14 23:19:45 +1200

    is shivering helper function

---
+++ src/main/java/com/github/thedeathlycow/frostiful/survival/SurvivalUtils.java
@@ -0,0 +1,17 @@
+package com.github.thedeathlycow.frostiful.survival;
+
+import com.github.thedeathlycow.frostiful.Frostiful;
+import com.github.thedeathlycow.frostiful.config.FrostifulConfig;
+import com.github.thedeathlycow.thermoo.api.temperature.TemperatureAware;
+
+public class SurvivalUtils {
+
+
+    public static boolean isShivering(TemperatureAware temperatureAware) {
+        FrostifulConfig config = Frostiful.getConfig();
+        return temperatureAware.thermoo$getTemperatureScale() < config.freezingConfig.getShiverBelow();
+    }
+
+    private SurvivalUtils() {
+    }
+}
