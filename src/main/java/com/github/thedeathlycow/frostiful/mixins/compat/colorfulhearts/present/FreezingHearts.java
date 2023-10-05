ea42c67b1888696976c2f50ccc0ecceaa58c3203
TheDeathlyCow <thedeathlycow@gmail.com>
2023-10-06 01:21:09 +1300

    Fix colorful hearts compat
    Updates the colorful hearts version to 4.0.1
    
    Fixes #51

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/compat/colorfulhearts/present/FreezingHearts.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/compat/colorfulhearts/present/FreezingHearts.java
@@ -2,6 +2,7 @@ package com.github.thedeathlycow.frostiful.mixins.compat.colorfulhearts.present;
 
 import com.github.thedeathlycow.frostiful.compat.FrostifulIntegrations;
 import net.minecraft.entity.player.PlayerEntity;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,16 +10,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import terrails.colorfulhearts.heart.HeartType;
 
 @Mixin(HeartType.class)
-public class FreezingHearts {
+public abstract class FreezingHearts {
 
     @Inject(
             method = "forPlayer",
             at = @At("TAIL"),
             cancellable = true
     )
-    private static void setFreezingHeartsWhenFrozen(PlayerEntity player, CallbackInfoReturnable<HeartType> cir) {
-        if (FrostifulIntegrations.isModLoaded(FrostifulIntegrations.OVERFLOWING_BARS_ID)) return;
+    private static void setFreezingHeartsWhenFrozen(
+            @NotNull PlayerEntity player, CallbackInfoReturnable<HeartType> cir
+    ) {
         if (player.thermoo$getTemperatureScale() <= -0.99) {
+            if (FrostifulIntegrations.isModLoaded(FrostifulIntegrations.OVERFLOWING_BARS_ID)) {
+                return;
+            }
             cir.setReturnValue(HeartType.FROZEN);
         }
     }
