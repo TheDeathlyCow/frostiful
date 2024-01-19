1f4b6788e3639fac5957e69797e92d31e55de5bd
TheDeathlyCow <thedeathlycow@gmail.com>
2024-01-18 20:32:44 -1000

    use tags for custom frost resistance materials

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/item/ArmorItemMixin.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/item/ArmorItemMixin.java
@@ -1,6 +1,7 @@
 package com.github.thedeathlycow.frostiful.mixins.item;
 
 import com.github.thedeathlycow.frostiful.item.FrostResistantArmorMaterial;
+import com.github.thedeathlycow.frostiful.item.attribute.FrostResistantArmorTagApplicator;
 import com.github.thedeathlycow.thermoo.api.ThermooAttributes;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
@@ -25,12 +26,7 @@ import java.util.UUID;
 public class ArmorItemMixin {
 
 
-    private static final EnumMap<ArmorItem.Type, UUID> frostiful$MODIFIERS = Util.make(new EnumMap<>(ArmorItem.Type.class), uuidMap -> {
-        uuidMap.put(ArmorItem.Type.BOOTS, UUID.fromString("43ce6852-f0e9-48b5-a451-f6d458f835a2"));
-        uuidMap.put(ArmorItem.Type.LEGGINGS, UUID.fromString("015a4e56-3db0-45e7-a14c-82ebc3af86b5"));
-        uuidMap.put(ArmorItem.Type.CHESTPLATE, UUID.fromString("99cdd997-74a7-4427-a272-4f2e65c7d5d1"));
-        uuidMap.put(ArmorItem.Type.HELMET, UUID.fromString("87102398-36e2-4704-91c4-f2af697928ec"));
-    });
+
 
     @Shadow
     @Final
@@ -42,7 +38,7 @@ public class ArmorItemMixin {
             at = @At("RETURN")
     )
     private void addFrostResistanceToFurLinedArmour(ArmorMaterial material, ArmorItem.Type type, Item.Settings settings, CallbackInfo ci) {
-        UUID uUID = frostiful$MODIFIERS.get(type);
+        UUID uUID = FrostResistantArmorTagApplicator.UUID_MAP.get(type);
 
         if (material instanceof FrostResistantArmorMaterial frostResistantArmorMaterial) {
             ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
