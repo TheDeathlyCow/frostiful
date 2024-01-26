6b03df141b20615933a0adaec24e2c1461f97b6f
TheDeathlyCow <thedeathlycow@gmail.com>
2024-01-25 18:20:42 -1000

    brushed polar bear texture
    Closes #63

--- src/main/java/com/github/thedeathlycow/frostiful/client/BrushableTextures.java
+++ src/main/java/com/github/thedeathlycow/frostiful/client/BrushableTextures.java
@@ -8,11 +8,7 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class BrushableTextures {
 
-
     public static final Identifier POLAR_BEAR = Frostiful.id("textures/entity/bear/polar_bear_brushed.png");
-
-    public static final Identifier OCELOT = Frostiful.id("textures/entity/bear/ocelot_brushed.png");
-    public static final Identifier WOLF = Frostiful.id("textures/entity/bear/wolf_brushed.png");
 
     private BrushableTextures() {
     }
