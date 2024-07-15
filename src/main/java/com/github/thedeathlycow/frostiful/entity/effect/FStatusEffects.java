187849173479dbaf366b27ff2c8f18979e9a907d
TheDeathlyCow <thedeathlycow@gmail.com>
2024-07-15 01:17:34 -1000

    fix status effects

--- src/main/java/com/github/thedeathlycow/frostiful/entity/effect/FStatusEffects.java
+++ src/main/java/com/github/thedeathlycow/frostiful/entity/effect/FStatusEffects.java
@@ -1,5 +1,6 @@
 package com.github.thedeathlycow.frostiful.entity.effect;
 
+import com.github.thedeathlycow.frostiful.Frostiful;
 import com.github.thedeathlycow.thermoo.api.ThermooAttributes;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.effect.StatusEffect;
@@ -10,21 +11,39 @@ import net.minecraft.registry.entry.RegistryEntry;
 
 public class FStatusEffects {
 
-    public static final String WARMTH_MODIFIER_ID = "c374d1af-dbea-4d2c-bbb1-7d1702a0ab6b";
-    public static final String FROST_BITE_MODIFIER_ID = "bd9f5aa5-584a-4700-a9ea-e1063dbd45cb";
 
-    public static final RegistryEntry<StatusEffect> WARMTH = new SimpleStatusEffect(StatusEffectCategory.BENEFICIAL, 0xE3963E)
-            .addAttributeModifier(ThermooAttributes.FROST_RESISTANCE, WARMTH_MODIFIER_ID, 1.0, EntityAttributeModifier.Operation.ADDITION);
+    public static final RegistryEntry<StatusEffect> WARMTH = registerReference(
+            "warmth",
+            new SimpleStatusEffect(
+                    StatusEffectCategory.BENEFICIAL, 0xE3963E
+            ).addAttributeModifier(
+                    ThermooAttributes.FROST_RESISTANCE,
+                    Frostiful.id("effect.warmth"),
+                    1.0,
+                    EntityAttributeModifier.Operation.ADD_VALUE
+            )
+    );
 
-    public static final RegistryEntry<StatusEffect> FROST_BITE = new SimpleStatusEffect(StatusEffectCategory.HARMFUL, 0x4287F5)
-            .addAttributeModifier(ThermooAttributes.FROST_RESISTANCE, FROST_BITE_MODIFIER_ID, -1.0, EntityAttributeModifier.Operation.ADDITION);
-
+    public static final RegistryEntry<StatusEffect> FROST_BITE = registerReference(
+            "warmth",
+            new SimpleStatusEffect(
+                    StatusEffectCategory.HARMFUL, 0x4287F5
+            ).addAttributeModifier(
+                    ThermooAttributes.FROST_RESISTANCE,
+                    Frostiful.id("effect.frost_base"),
+                    -1.0,
+                    EntityAttributeModifier.Operation.ADD_VALUE
+            )
+    );
 
     public static void initialize() {
         // load this class
     }
 
-    private static RegistryEntry<StatusEffect> register(String name, StatusEffect statusEffect) {
-        return Registry.registerReference(Registries.STATUS_EFFECT, Scorchful.id(name), statusEffect);
+    private static RegistryEntry<StatusEffect> registerReference(String name, StatusEffect statusEffect) {
+        return Registry.registerReference(Registries.STATUS_EFFECT, Frostiful.id(name), statusEffect);
+    }
+
+    private FStatusEffects() {
     }
 }
