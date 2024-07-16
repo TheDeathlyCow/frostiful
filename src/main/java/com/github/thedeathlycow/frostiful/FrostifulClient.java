39e72d0810569536b39b7ae3a0e9643a373d6102
TheDeathlyCow <thedeathlycow@gmail.com>
2024-07-15 21:15:54 -1000

    fix frostologer eyes renderer

--- src/main/java/com/github/thedeathlycow/frostiful/FrostifulClient.java
+++ src/main/java/com/github/thedeathlycow/frostiful/FrostifulClient.java
@@ -14,7 +14,6 @@ import com.github.thedeathlycow.thermoo.api.client.StatusBarOverlayRenderEvents;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
