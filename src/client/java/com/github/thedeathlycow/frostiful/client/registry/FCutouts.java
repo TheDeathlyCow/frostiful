f0d64ad735da3b4bec8044f48d7fb804c631df4d
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-18 20:55:06 -1000

    update block render layers

--- src/client/java/com/github/thedeathlycow/frostiful/client/registry/FCutouts.java
+++ src/client/java/com/github/thedeathlycow/frostiful/client/registry/FCutouts.java
@@ -3,24 +3,24 @@ package com.github.thedeathlycow.frostiful.client.registry;
 import com.github.thedeathlycow.frostiful.registry.FBlocks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class FCutouts {
 
     public static void initialize() {
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.ICICLE, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.COLD_SUN_LICHEN, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.COOL_SUN_LICHEN, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.WARM_SUN_LICHEN, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.HOT_SUN_LICHEN, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.FROZEN_TORCH, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.FROZEN_WALL_TORCH, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.ICE_PANE, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.ICY_TRIAL_SPAWNER, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.ICY_VAULT, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(FBlocks.BRITTLE_ICE, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.putBlock(FBlocks.ICICLE, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.COLD_SUN_LICHEN, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.COOL_SUN_LICHEN, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.WARM_SUN_LICHEN, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.HOT_SUN_LICHEN, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.FROZEN_TORCH, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.FROZEN_WALL_TORCH, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.ICE_PANE, BlockRenderLayer.TRANSLUCENT);
+        BlockRenderLayerMap.putBlock(FBlocks.ICY_TRIAL_SPAWNER, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.ICY_VAULT, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(FBlocks.BRITTLE_ICE, BlockRenderLayer.TRANSLUCENT);
     }
 
     private FCutouts() {
