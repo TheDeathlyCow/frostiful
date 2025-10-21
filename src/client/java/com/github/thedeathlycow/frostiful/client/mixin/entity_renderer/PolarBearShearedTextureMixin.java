a8048673b57eb5529d712e59ef20f92144b68a80
TheDeathlyCow <thedeathlycow@gmail.com>
2025-10-21 02:06:56 -1000

    disable hurt polar bear skin
    resolves #191

--- src/client/java/com/github/thedeathlycow/frostiful/client/mixin/entity_renderer/PolarBearShearedTextureMixin.java
+++ src/client/java/com/github/thedeathlycow/frostiful/client/mixin/entity_renderer/PolarBearShearedTextureMixin.java
@@ -1,28 +1,27 @@
 package com.github.thedeathlycow.frostiful.client.mixin.entity_renderer;
 
+import com.github.thedeathlycow.frostiful.Frostiful;
 import com.github.thedeathlycow.frostiful.client.BrushableTextures;
 import com.github.thedeathlycow.frostiful.registry.FComponents;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.PolarBearEntityRenderer;
 import net.minecraft.entity.passive.PolarBearEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PolarBearEntityRenderer.class)
 @Environment(EnvType.CLIENT)
 public class PolarBearShearedTextureMixin {
-    @Inject(
-            method = "getTexture(Lnet/minecraft/entity/passive/PolarBearEntity;)Lnet/minecraft/util/Identifier;",
-            at = @At("HEAD"),
-            cancellable = true
+    @WrapMethod(
+            method = "getTexture(Lnet/minecraft/entity/passive/PolarBearEntity;)Lnet/minecraft/util/Identifier;"
     )
-    public void getShearedTexture(PolarBearEntity polarBear, CallbackInfoReturnable<Identifier> cir) {
-        if (FComponents.BRUSHABLE_COMPONENT.get(polarBear).wasBrushed()) {
-            cir.setReturnValue(BrushableTextures.POLAR_BEAR);
+    public Identifier getShearedTexture(PolarBearEntity polarBear, Operation<Identifier> original) {
+        if (!Frostiful.getConfig().clientConfig.isDisableHurtPolarBearSkin() && FComponents.BRUSHABLE_COMPONENT.get(polarBear).wasBrushed()) {
+            return BrushableTextures.POLAR_BEAR;
         }
+        return original.call(polarBear);
     }
 }
