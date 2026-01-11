5fc967f079ae5740cff679523a3a4588fb203dd7
TheDeathlyCow <thedeathlycow@gmail.com>
2026-01-11 02:39:30 -1000

    item tags

--- src/datagen/java/com/github/thedeathlycow/frostiful/datagen/FrostifulDataGenerator.java
+++ src/datagen/java/com/github/thedeathlycow/frostiful/datagen/FrostifulDataGenerator.java
@@ -2,6 +2,7 @@ package com.github.thedeathlycow.frostiful.datagen;
 
 import com.github.thedeathlycow.frostiful.Frostiful;
 import com.github.thedeathlycow.frostiful.datagen.generator.FRecipeProvider;
+import com.github.thedeathlycow.frostiful.datagen.generator.ItemTagGenerator;
 import com.github.thedeathlycow.frostiful.datagen.generator.client.FrostifulModelGenerator;
 import com.github.thedeathlycow.frostiful.registry.FArmorTrimPatterns;
 import com.github.thedeathlycow.frostiful.registry.FBannerPatterns;
@@ -26,6 +27,7 @@ public class FrostifulDataGenerator implements DataGeneratorEntrypoint {
 
         pack.addProvider(FRecipeProvider::new);
         pack.addProvider(FrostifulModelGenerator::new);
+        pack.addProvider(ItemTagGenerator::new);
     }
 
     @Override
---
+++ src/datagen/java/com/github/thedeathlycow/frostiful/datagen/generator/ItemTagGenerator.java
@@ -0,0 +1,132 @@
+package com.github.thedeathlycow.frostiful.datagen.generator;
+
+import com.github.thedeathlycow.frostiful.registry.FItems;
+import com.github.thedeathlycow.frostiful.registry.tag.FItemTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
+
+    public ItemTagGenerator(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider wrapperLookup) {
+        generateFrostifulTags(wrapperLookup);
+        generateConventionalTags(wrapperLookup);
+    }
+
+    private void generateFrostifulTags(HolderLookup.Provider wrapperLookup) {
+        valueLookupBuilder(FItemTags.ENCHANTABLE_ICE_SKATES)
+                .addOptionalTag(FItemTags.ICE_SKATES);
+
+        valueLookupBuilder(FItemTags.ENCHANTABLE_FROST_WAND)
+                .add(FItems.FROST_WAND);
+
+        valueLookupBuilder(FItemTags.FUR_ARMOR)
+                .add(FItems.FUR_HELMET)
+                .add(FItems.FUR_CHESTPLATE)
+                .add(FItems.FUR_LEGGINGS)
+                .add(FItems.FUR_BOOTS)
+                .add(FItems.FUR_PADDED_CHAINMAIL_HELMET)
+                .add(FItems.FUR_PADDED_CHAINMAIL_CHESTPLATE)
+                .add(FItems.FUR_PADDED_CHAINMAIL_LEGGINGS)
+                .add(FItems.FUR_PADDED_CHAINMAIL_BOOTS);
+
+        valueLookupBuilder(FItemTags.FUR_BOOTS)
+                .add(FItems.FUR_BOOTS)
+                .add(FItems.FUR_PADDED_CHAINMAIL_BOOTS);
+
+        valueLookupBuilder(FItemTags.ICE_SKATES)
+                .add(FItems.ICE_SKATES)
+                .add(FItems.ARMORED_ICE_SKATES);
+
+        valueLookupBuilder(FItemTags.FUR_TUFTS)
+                .add(FItems.POLAR_BEAR_FUR_TUFT)
+                .add(FItems.WOLF_FUR_TUFT)
+                .add(FItems.OCELOT_FUR_TUFT)
+                .add(Items.RABBIT_HIDE);
+
+        valueLookupBuilder(FItemTags.ICICLES)
+                .add(FItems.ICICLE)
+                .addOptionalTag(commonKey("icicles"));
+
+        getOrCreateRawBuilder(FItemTags.ICICLES)
+                .addOptionalElement(Identifier.fromNamespaceAndPath("immersive_weathering", "icicle"));
+
+        valueLookupBuilder(FItemTags.POWDER_SNOW_WALKABLE)
+                .add(Items.LEATHER_BOOTS)
+                .add(FItems.FUR_BOOTS)
+                .add(FItems.FUR_PADDED_CHAINMAIL_BOOTS)
+                .add(Items.LEATHER_HORSE_ARMOR);
+
+        valueLookupBuilder(FItemTags.REPAIRS_FROST_WAND)
+                .add(FItems.FROZEN_ROD);
+
+        valueLookupBuilder(FItemTags.REPAIRS_FUR_ARMOR)
+                .addOptionalTag(FItemTags.FUR_TUFTS);
+
+        valueLookupBuilder(FItemTags.REPAIRS_FUR_LINED_CHAINMAIL_ARMOR)
+                .add(Items.IRON_INGOT)
+                .addOptionalTag(ConventionalItemTags.IRON_INGOTS);
+
+        valueLookupBuilder(FItemTags.SUN_LICHENS)
+                .add(FItems.COLD_SUN_LICHEN)
+                .add(FItems.COOL_SUN_LICHEN)
+                .add(FItems.WARM_SUN_LICHEN)
+                .add(FItems.HOT_SUN_LICHEN);
+
+        valueLookupBuilder(FItemTags.SUPPORTS_HEAT_DRAIN)
+                .addOptionalTag(FItemTags.ENCHANTABLE_FROST_WAND)
+                .addOptionalTag(ItemTags.WEAPON_ENCHANTABLE);
+    }
+
+    private void generateConventionalTags(HolderLookup.Provider wrapperLookup) {
+        valueLookupBuilder(ConventionalItemTags.MELEE_WEAPON_TOOLS)
+                .add(FItems.FROST_WAND);
+
+        valueLookupBuilder(ConventionalItemTags.RANGED_WEAPON_TOOLS)
+                .add(FItems.FROST_WAND);
+
+        valueLookupBuilder(ConventionalItemTags.SPEAR_TOOLS)
+                .add(FItems.FROST_WAND);
+
+        valueLookupBuilder(ConventionalItemTags.GLASS_PANES)
+                .add(FItems.ICE_PANE);
+
+        valueLookupBuilder(ConventionalItemTags.GLASS_BLOCKS_COLORLESS)
+                .add(FItems.ICE_PANE);
+
+        valueLookupBuilder(commonKey("icicles"))
+                .add(FItems.ICICLE);
+
+        valueLookupBuilder(ConventionalItemTags.HUMANOID_ARMORS)
+                .addOptionalTag(FItemTags.FUR_ARMOR);
+
+        valueLookupBuilder(ConventionalItemTags.ENCHANTABLES)
+                .addOptionalTag(FItemTags.ENCHANTABLE_FROST_WAND)
+                .addOptionalTag(FItemTags.ENCHANTABLE_ICE_SKATES);
+
+        valueLookupBuilder(ConventionalItemTags.RODS)
+                .add(FItems.FROZEN_ROD);
+    }
+
+    private static TagKey<Item> commonKey(String path) {
+        return key("c", path);
+    }
+
+    private static TagKey<Item> key(String id, String path) {
+        return TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(id, path));
+    }
+}
