fa74d807cff897b6ad2d342f301762dfa0b509a9
TheDeathlyCow <thedeathlycow@gmail.com>
2026-01-12 21:49:17 -1000

    vanilla block tags

--- src/datagen/java/com/github/thedeathlycow/frostiful/datagen/generator/tag/FBlockTagGenerator.java
+++ src/datagen/java/com/github/thedeathlycow/frostiful/datagen/generator/tag/FBlockTagGenerator.java
@@ -1,11 +1,19 @@
 package com.github.thedeathlycow.frostiful.datagen.generator.tag;
 
 import com.github.thedeathlycow.frostiful.registry.FBlocks;
+import com.github.thedeathlycow.frostiful.registry.FItems;
 import com.github.thedeathlycow.frostiful.registry.tag.FBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +25,81 @@ public class FBlockTagGenerator extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider wrapperLookup) {
+        generateMinecraftTags(wrapperLookup);
+        generateConventionalTags(wrapperLookup);
         generateFrostifulTags(wrapperLookup);
+    }
+
+    private void generateMinecraftTags(HolderLookup.Provider wrapperLookup) {
+        valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(FBlocks.ICICLE)
+                .add(FBlocks.PACKED_SNOW)
+                .add(FBlocks.PACKED_SNOW_BLOCK)
+                .add(FBlocks.PACKED_SNOW_BRICKS)
+                .add(FBlocks.PACKED_SNOW_BRICK_STAIRS)
+                .add(FBlocks.PACKED_SNOW_BRICK_SLAB)
+                .add(FBlocks.PACKED_SNOW_BRICK_WALL)
+                .add(FBlocks.CUT_PACKED_ICE)
+                .add(FBlocks.CUT_PACKED_ICE_SLAB)
+                .add(FBlocks.CUT_PACKED_ICE_STAIRS)
+                .add(FBlocks.CUT_PACKED_ICE_WALL)
+                .add(FBlocks.CUT_BLUE_ICE)
+                .add(FBlocks.CUT_BLUE_ICE_SLAB)
+                .add(FBlocks.CUT_BLUE_ICE_STAIRS)
+                .add(FBlocks.CUT_BLUE_ICE_WALL)
+                .add(FBlocks.BRITTLE_ICE);
+
+        valueLookupBuilder(BlockTags.FEATURES_CANNOT_REPLACE)
+                .add(FBlocks.ICY_VAULT)
+                .add(FBlocks.ICY_TRIAL_SPAWNER);
+
+        valueLookupBuilder(BlockTags.ICE)
+                .add(FBlocks.ICE_PANE)
+                .add(FBlocks.CUT_PACKED_ICE)
+                .add(FBlocks.CUT_PACKED_ICE_SLAB)
+                .add(FBlocks.CUT_PACKED_ICE_STAIRS)
+                .add(FBlocks.CUT_PACKED_ICE_WALL)
+                .add(FBlocks.CUT_BLUE_ICE)
+                .add(FBlocks.CUT_BLUE_ICE_SLAB)
+                .add(FBlocks.CUT_BLUE_ICE_STAIRS)
+                .add(FBlocks.CUT_BLUE_ICE_WALL)
+                .add(FBlocks.BRITTLE_ICE);
+
+        valueLookupBuilder(BlockTags.INSIDE_STEP_SOUND_BLOCKS)
+                .addOptionalTag(FBlockTags.SUN_LICHENS);
+
+        valueLookupBuilder(BlockTags.SLABS)
+                .add(FBlocks.PACKED_SNOW_BRICK_SLAB)
+                .add(FBlocks.CUT_PACKED_ICE_SLAB)
+                .add(FBlocks.CUT_BLUE_ICE_SLAB);
+
+        valueLookupBuilder(BlockTags.SNOW)
+                .add(FBlocks.PACKED_SNOW)
+                .add(FBlocks.PACKED_SNOW_BLOCK);
+
+        valueLookupBuilder(BlockTags.STAIRS)
+                .add(FBlocks.PACKED_SNOW_BRICK_STAIRS)
+                .add(FBlocks.CUT_PACKED_ICE_STAIRS)
+                .add(FBlocks.CUT_BLUE_ICE_STAIRS);
+
+        valueLookupBuilder(BlockTags.WALL_POST_OVERRIDE)
+                .add(FBlocks.FROZEN_TORCH);
+
+        valueLookupBuilder(BlockTags.WALLS)
+                .add(FBlocks.PACKED_SNOW_BRICK_WALL)
+                .add(FBlocks.CUT_PACKED_ICE_WALL)
+                .add(FBlocks.CUT_BLUE_ICE_WALL);
+    }
+
+    private void generateConventionalTags(HolderLookup.Provider wrapperLookup) {
+        valueLookupBuilder(ConventionalBlockTags.GLASS_PANES)
+                .add(FBlocks.ICE_PANE);
+
+        valueLookupBuilder(ConventionalBlockTags.GLASS_BLOCKS_COLORLESS)
+                .add(FBlocks.ICE_PANE);
+
+        valueLookupBuilder(commonKey("icicles"))
+                .add(FBlocks.ICICLE);
     }
 
     private void generateFrostifulTags(HolderLookup.Provider wrapperLookup) {
@@ -71,4 +153,11 @@ public class FBlockTagGenerator extends FabricTagProvider.BlockTagProvider {
                 .add(FBlocks.HOT_SUN_LICHEN);
     }
 
+    private static TagKey<Block> commonKey(String path) {
+        return key("c", path);
+    }
+
+    private static TagKey<Block> key(String id, String path) {
+        return TagKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(id, path));
+    }
 }
