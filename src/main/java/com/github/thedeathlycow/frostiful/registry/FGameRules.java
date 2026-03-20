b92c274433a4e60796afbf82842c92326f42a578
TheDeathlyCow <thedeathlycow@gmail.com>
2026-03-19 19:00:02 -1000

    change do passive freezing gamerule to enable environment freezing with alias

--- src/main/java/com/github/thedeathlycow/frostiful/registry/FGameRules.java
+++ src/main/java/com/github/thedeathlycow/frostiful/registry/FGameRules.java
@@ -1,40 +1,38 @@
 package com.github.thedeathlycow.frostiful.registry;
 
 import com.github.thedeathlycow.frostiful.Frostiful;
-import com.github.thedeathlycow.frostiful.util.TextStyles;
-import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
-import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.gamerules.GameRule;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
 
 /**
- * Custom game rules for Lost in the Cold
+ * Custom game rules for Frostiful
  */
 public class FGameRules {
-
     /**
      * Mod's dedicated category for game rules
      */
-    public static final CustomGameRuleCategory CATEGORY = new CustomGameRuleCategory(
-            Frostiful.id("gamerule_category"),
-            Component.translatable("gamerule.category." + Frostiful.MODID)
-                    .setStyle(TextStyles.GAME_RULE_TITLE)
-    );
+    public static final GameRuleCategory SURVIVAL_CATEGORY = GameRuleCategory.register(Frostiful.id("survival"));
 
     /**
      * Whether to apply passive freezing. When false, player freezing will revert
      * to vanilla, except for the increased frost resistance provided by their
      * frost resistance attribute.
      */
-    public static final GameRule<Boolean> DO_PASSIVE_FREEZING =
+    public static final GameRule<Boolean> ENABLE_ENVIRONMENT_FREEZING =
             GameRuleBuilder.forBoolean(true)
-                    .category(CATEGORY)
-                    .buildAndRegister(Frostiful.id("do_passive_freezing"));
+                    .category(SURVIVAL_CATEGORY)
+                    .buildAndRegister(Frostiful.id("enable_environment_freezing"));
 
     public static void initialize() {
         Frostiful.LOGGER.debug("Initialized Frostiful game rules");
+        BuiltInRegistries.GAME_RULE.addAlias(
+                Frostiful.id("do_passive_freezing"),
+                Frostiful.id("enable_environment_freezing")
+        );
     }
-
+    
     private FGameRules() {
 
     }
