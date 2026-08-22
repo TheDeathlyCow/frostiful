bb9482f5a091b83370e7b1a9e08dc821086d89cc
TheDeathlyCow <thedeathlycow@gmail.com>
2026-08-21 23:32:09 -1000

    fix colorful hearts integration

--- src/main/java/com/github/thedeathlycow/frostiful/client/compat/ColorfulHeartsIntegration.java
+++ src/main/java/com/github/thedeathlycow/frostiful/client/compat/ColorfulHeartsIntegration.java
@@ -4,13 +4,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.NeoForge;
 import terrails.colorfulhearts.api.heart.Hearts;
 import terrails.colorfulhearts.api.heart.drawing.OverlayHeart;
 import terrails.colorfulhearts.api.neoforge.event.NeoHeartRenderEvent;
 
 public class ColorfulHeartsIntegration {
     public static void initialize(IEventBus modBus) {
-        modBus.addListener(ColorfulHeartsIntegration::preRender);
+        NeoForge.EVENT_BUS.addListener(ColorfulHeartsIntegration::preRender);
     }
 
     private static void preRender(NeoHeartRenderEvent.Pre event) {
